c421d3bda092c2af93f97543ce403e566f6baaff
Paul Zupan <pzupan@gmail.com>
2018-08-27 14:54:41 -0700

    add active storage migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_142032) do
+ActiveRecord::Schema.define(version: 2018_08_27_215403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -31,6 +31,27 @@ ActiveRecord::Schema.define(version: 2018_08_21_142032) do
     t.index ["user_id"], name: "index_accounts_on_user_id"
     t.index ["username", "provider"], name: "index_accounts_on_username_and_provider", unique: true
     t.index ["username"], name: "index_accounts_on_username"
+  end
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "categories", id: :serial, force: :cascade do |t|
