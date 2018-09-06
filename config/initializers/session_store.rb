f8a3722b90bcc28443ac503608ed8a28bf0d5626
pzupan@gmail.com <pzupan@gmail.com>
2018-09-06 14:17:36 -0700

    add domain all to session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-BonsaiAssetIndex::Application.config.session_store :cookie_store, key: '_bonsai_asset_index_session'
+BonsaiAssetIndex::Application.config.session_store :cookie_store, key: '_bonsai_asset_index_session', domain: :all
