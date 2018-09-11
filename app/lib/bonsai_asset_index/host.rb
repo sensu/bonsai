5663dffd294a393461fb95301c4eca599656e433
pzupan@gmail.com <pzupan@gmail.com>
2018-09-11 15:30:54 -0700

    fix bug in host file

--- app/lib/bonsai_asset_index/host.rb
+++ app/lib/bonsai_asset_index/host.rb
@@ -1,7 +1,7 @@
 module BonsaiAssetIndex
   module Host
     def self.full_url
-      if ENV['APP_PORT'].present? && !%w(80 443).include?(ENV['APP_PORT'])
+      if !Rails.env.production? && ENV['APP_PORT'].present? && !%w(80 443).include?(ENV['APP_PORT'])
         "#{ENV['PROTOCOL']}://#{ENV['HOST']}:#{ENV['APP_PORT']}"
       else
         "#{ENV['PROTOCOL']}://#{ENV['HOST']}"
