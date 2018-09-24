0f25e16e9962127b5dde9c4408f788c0d86323d5
Dan Milliron <dan@railsblazer.com>
2018-09-23 18:09:13 -0600

    User feature_sign_in/out for feature tests

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -2,15 +2,15 @@ require 'spec_feature_helper'
 
 describe 'signing in with oauth' do
   it 'signs a user in' do
-    sign_in(create(:user))
+    feature_sign_in(create(:user))
     expect_to_see_success_message
   end
 end
 
 describe 'signing out with oc-id' do
   it 'signs a user out' do
-    sign_in(create(:user))
-    sign_out
+    feature_sign_in(create(:user))
+    feature_sign_out
     expect_to_see_success_message
   end
 end
