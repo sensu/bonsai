2563bd73565972410c577170c65f87b63cbd9fdd
Dan Milliron <dan@railsblazer.com>
2018-09-26 16:01:26 -0600

    Add a controller test for extension_versions#download

---
+++ spec/controllers/extension_versions_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe ExtensionVersionsController do
+  include ExtensionVersionsHelper
+
+  let(:extension_version) { create :extension_version }
+  let(:extension)         { extension_version.extension }
+
+  before do
+    sign_in create(:user)
+  end
+
+  describe "download" do
+    it 'succeeds' do
+      get :download, params: {username: extension.owner_name, extension_id: extension.lowercase_name, version: extension_version.version}
+
+      expect(response).to redirect_to extension_version.download_url
+    end
+  end
+end
