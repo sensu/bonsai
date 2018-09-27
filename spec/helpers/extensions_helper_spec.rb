ab5b58daeaa3ae3da3d5836769726f03ba1b230e
Dan Milliron <dan@railsblazer.com>
2018-09-26 23:09:02 -0600

    Hide the commits sparkline for hosted extensions

--- spec/helpers/extensions_helper_spec.rb
+++ spec/helpers/extensions_helper_spec.rb
@@ -14,4 +14,16 @@ describe ExtensionsHelper do
       it {expect(helper.is_followable?(extension)).to be_falsey}
     end
   end
+
+  describe "is_commitable?" do
+    context "a GitHub-based extension" do
+      it {expect(helper.is_commitable?(extension)).to be_truthy}
+    end
+
+    context "a hosted extension" do
+      let(:extension) { build_stubbed :extension, :hosted }
+
+      it {expect(helper.is_commitable?(extension)).to be_falsey}
+    end
+  end
 end
