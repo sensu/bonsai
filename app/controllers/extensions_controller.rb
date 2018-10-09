82d34dce84ffea819abb639f8833afc36461da31
Dan Milliron <dan@railsblazer.com>
2018-10-09 13:50:29 -0600

    Add a GUI field for the hosted extension version

--- app/controllers/extensions_controller.rb
+++ app/controllers/extensions_controller.rb
@@ -75,7 +75,7 @@ class ExtensionsController < ApplicationController
   # Create an extension.
   #
   def create
-    eparams = params.require(:extension).permit(:name, :description, :github_url, :tmp_source_file, :tag_tokens, compatible_platforms: [])
+    eparams = params.require(:extension).permit(:name, :description, :github_url, :tmp_source_file, :tag_tokens, :version, compatible_platforms: [])
     create_extension = CreateExtension.new(eparams, current_user)
     @extension = create_extension.process!
 
