b0b23720c2479988e0a44aaebf55e4557b105e96
Dan Milliron <dan@railsblazer.com>
2018-11-30 02:05:26 -0700

    Add the Extension.in_namespace scope
    to support filtering the results of the API endpoint for listing extensions.

--- app/models/extension.rb
+++ app/models/extension.rb
@@ -74,6 +74,10 @@ class Extension < ApplicationRecord
     joins(owner: :github_account).where('accounts.username = ?', username)
   }
 
+  scope :in_namespace, lambda { |namespace|
+    where(owner_name: namespace)
+  }
+
   scope :supported_platforms, lambda { |sp_ids|
     joins(:all_supported_platforms).where('supported_platforms.id IN (?)', sp_ids)
   }
