d887c21d0b68de4db78b4f9a078c361b48fcbe6b
Dan Milliron <dan@railsblazer.com>
2018-11-30 02:05:41 -0700

    Customize the apipie config

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,7 +1,11 @@
 Apipie.configure do |config|
-  config.app_name                = "BonsaiAssetIndex"
-  config.api_base_url            = "/api"
-  config.doc_base_url            = "/apipie"
+  config.app_name                = Rails.configuration.app_name
+  config.api_base_url            = "/api/v1"
+  config.doc_base_url            = "/apidoc"
   # where is your API defined?
-  config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
+  config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/v1/**/*.rb"
+
+  config.validate  = false
+  config.translate = false
+  config.markup    = Apipie::Markup::Markdown.new
 end
