86502b87b25b04c1af50a24358d543dcde1af43b
Dan Milliron <dan@railsblazer.com>
2018-12-12 08:10:20 -0700

    Support the #{repo} variable in extension config files

--- spec/interactors/compile_extension_version_config_spec.rb
+++ spec/interactors/compile_extension_version_config_spec.rb
@@ -2,21 +2,23 @@ require "spec_helper"
 
 describe CompileExtensionVersionConfig do
   let(:version_name) { "1.2.2"}
+  let(:repo_name)    { "my_repo" }
   let(:config)       { {"builds"=>
                           [{"arch"=>"x86_64",
                             "filter"=>
                               ["System.OS == linux",
                                "(System.Arch == x86_64) || (System.Arch == amd64)"],
                             "platform"=>"linux",
-                            "sha_filename"=>"test_asset-\#{version}-linux-x86_64.sha512.txt",
-                            "asset_filename"=>"test_asset-\#{version}-linux-x86_64.tar.gz"}]} }
-  let(:version)      { create :extension_version, config: config, version: version_name }
+                            "sha_filename"=>"\#{repo}-\#{version}-linux-x86_64.sha512.txt",
+                            "asset_filename"=>"\#{repo}-\#{version}-linux-x86_64.tar.gz"}]} }
+  let(:extension)    { create :extension, extension_versions_count: 0, github_url: "https://github.com/owner/#{repo_name}" }
+  let(:version)      { create :extension_version, extension: extension, config: config, version: version_name }
   subject(:context)  { CompileExtensionVersionConfig.call(version: version) }
 
   describe ".call" do
-    let(:asset_hash1)        { {name: "test_asset-1.2.2-linux-x86_64.tar.gz",
+    let(:asset_hash1)        { {name: "my_repo-1.2.2-linux-x86_64.tar.gz",
                                 browser_download_url: "https://example.com/download"} }
-    let(:asset_hash2)        { {name: "test_asset-1.2.2-linux-x86_64.sha512.txt",
+    let(:asset_hash2)        { {name: "my_repo-1.2.2-linux-x86_64.sha512.txt",
                                 browser_download_url: "https://example.com/sha_download"} }
     let(:release_data)       { {tag_name: version.version, assets: [asset_hash1, asset_hash2]} }
     let(:expected_data_hash) { {"builds" =>
@@ -25,8 +27,8 @@ describe CompileExtensionVersionConfig do
                                       ["System.OS == linux",
                                        "(System.Arch == x86_64) || (System.Arch == amd64)"],
                                     "platform"       => "linux",
-                                    "sha_filename"   => "test_asset-\#{version}-linux-x86_64.sha512.txt",
-                                    "asset_filename" => "test_asset-\#{version}-linux-x86_64.tar.gz",
+                                    "sha_filename"   => "\#{repo}-\#{version}-linux-x86_64.sha512.txt",
+                                    "asset_filename" => "\#{repo}-\#{version}-linux-x86_64.tar.gz",
                                     "asset_url"      => "https://example.com/download",
                                     "asset_sha"      => "c1ec2f493f0ff9d83914c1ec2f493f0ff9d83914"}]} }
 
