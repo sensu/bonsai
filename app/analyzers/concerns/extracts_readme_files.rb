65b17bbcfb57ff881c1ebf9a9f9a06b2fe135696
Dan Milliron <dan@railsblazer.com>
2019-01-10 10:02:15 -0700

    Extend the ActiveStorage analyzers to fetch arbitrary files

--- app/analyzers/concerns/extracts_readme_files.rb
+++ app/analyzers/concerns/extracts_readme_files.rb
@@ -8,7 +8,7 @@ module ExtractsReadmeFiles
   def extract_readme(files:, path_method:, file_reader:)
     full_paths       = files.map(&path_method)
     readme_paths     = full_paths.grep /\/readme/i
-    best_readme_path = readme_paths.sort_by { |path| path.split('/').size }.first
+    best_readme_path = readme_paths.sort_by { |path| [path.split('/').size, File.basename(path).size] }.first
 
     # Don't go deeper than one level down, e.g. foo/readme is fine, but foo/down/readme is not.
     return [nil, nil] if best_readme_path.split('/').size > 2
@@ -16,5 +16,16 @@ module ExtractsReadmeFiles
     content   = file_reader.call(files, best_readme_path).presence
     extension = File.extname(best_readme_path).to_s.sub(/\A\./, '').presence  # strip off any leading '.'
     return [content, extension]
+  end
+
+  def extract_file(file_path:, files:, path_method:, file_reader:)
+    full_paths     = files.map(&path_method)
+    matching_paths = full_paths.grep /#{file_path}\z/
+    best_path      = matching_paths.sort_by { |path| [path.split('/').size, File.basename(path).size] }.first
+
+    # Don't go deeper than one level down, e.g. foo/<file_path> is fine, but foo/down/readme is not.
+    return nil if best_path.split('/').size > file_path.split('/').size + 1
+
+    file_reader.call(files, best_path).presence
   end
 end
