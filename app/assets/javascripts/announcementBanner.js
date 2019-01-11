dc36f4dceef7283af4a1d8240ffb0d20a186190f
Dan Milliron <dan@railsblazer.com>
2019-01-11 01:17:37 -0700

    Add a close button to the banner announcement

--- app/assets/javascripts/announcementBanner.js
+++ app/assets/javascripts/announcementBanner.js
@@ -1,4 +1,8 @@
 $(function() {
+  $(".announcement_banner_header .close-reveal-modal-x").click(function() {
+    $(".announcement_banner_header").slideUp(240);
+  });
+
   $(".announcement_banner_header.clickable").click(function() {
     if ($(".announcement_banner_content").is(":hidden")) {
       $(".announcement_banner_content").slideDown(300);
