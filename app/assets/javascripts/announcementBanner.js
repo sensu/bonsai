2070f05bd5de374e7262a8b992e6d491702f405a
Dan Milliron <dan@railsblazer.com>
2019-01-11 01:18:10 -0700

    Use a cookie to remember if the user has closed the announcement banner
    as per https://trello.com/c/haUMwBcp

--- app/assets/javascripts/announcementBanner.js
+++ app/assets/javascripts/announcementBanner.js
@@ -1,6 +1,10 @@
 $(function() {
+  if (getCookie("no_announcement_banner") == "beta_launch") {
+    $(".announcement_banner_header").hide();
+  }
   $(".announcement_banner_header .close-reveal-modal-x").click(function() {
     $(".announcement_banner_header").slideUp(240);
+    document.cookie = "no_announcement_banner=beta_launch; max_age=31536000; path=/"
   });
 
   $(".announcement_banner_header.clickable").click(function() {
@@ -11,3 +15,19 @@ $(function() {
     }
   });
 });
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
