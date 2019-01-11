624c91eeb9099b9b244975a0d427ddf28f896a1b
Dan Milliron <dan@railsblazer.com>
2019-01-10 20:24:30 -0700

    Add file upload progress indicator

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,7 @@
 //= require foundation.min
 //= require jquery.slugit
 //= require checkbox
+//= require direct_uploads
 //= require extensionShow
 //= require extensionFollowing
 //= require joinOrganization
