d10505c0048a7167b2ff09ae6b811986d82ead84
Dan Milliron <dan@railsblazer.com>
2019-01-13 00:10:53 -0700

    Extend #extract_file to accept Regexp paths

--- app/analyzers/concerns/extracts_files.rb
+++ app/analyzers/concerns/extracts_files.rb
@@ -20,11 +20,15 @@ module ExtractsFiles
 
   def extract_file(file_path:, files:, path_method:, file_reader:)
     full_paths     = files.map(&path_method)
-    matching_paths = full_paths.grep /#{file_path}\z/
+    regexp         = file_path.is_a?(Regexp) ? file_path : /#{file_path}\z/
+    matching_paths = full_paths.grep regexp
     best_path      = matching_paths.sort_by { |path| [path.split('/').size, File.basename(path).size] }.first
 
     # Don't go deeper than one level down, e.g. foo/<file_path> is fine, but foo/down/<file_path> is not.
-    return [nil, nil] if best_path.split('/').size > file_path.split('/').size + 1
+    split_file_path = file_path.to_s.split('/')
+    # Handle Regexps that begin with /, e.g. /\/readme/i
+    split_file_path.shift if file_path.is_a?(Regexp) && split_file_path.first =~ /\\\z/
+    return [nil, nil] if best_path.split('/').size > split_file_path.size + 1
 
     content = file_reader.call(files, best_path).presence
     return content, best_path
