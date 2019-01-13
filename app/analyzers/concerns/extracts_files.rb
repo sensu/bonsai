7ba7e5670aa32796247f12d2b52eb65835516de8
Dan Milliron <dan@railsblazer.com>
2019-01-13 00:16:51 -0700

    Refactor code; no functional change

--- app/analyzers/concerns/extracts_files.rb
+++ app/analyzers/concerns/extracts_files.rb
@@ -6,16 +6,9 @@ module ExtractsFiles
   private
 
   def extract_readme(files:, path_method:, file_reader:)
-    full_paths       = files.map(&path_method)
-    readme_paths     = full_paths.grep /\/readme/i
-    best_readme_path = readme_paths.sort_by { |path| [path.split('/').size, File.basename(path).size] }.first
-
-    # Don't go deeper than one level down, e.g. foo/readme is fine, but foo/down/readme is not.
-    return [nil, nil] if best_readme_path.split('/').size > 2
-
-    content   = file_reader.call(files, best_readme_path).presence
-    extension = File.extname(best_readme_path).to_s.sub(/\A\./, '').presence  # strip off any leading '.'
-    return [content, extension]
+    content, path = extract_file(file_path: /\/readme/i, files: files, path_method: path_method, file_reader: file_reader)
+    extension = File.extname(path.to_s).to_s.sub(/\A\./, '').presence  # strip off any leading '.'
+    return content, extension
   end
 
   def extract_file(file_path:, files:, path_method:, file_reader:)
