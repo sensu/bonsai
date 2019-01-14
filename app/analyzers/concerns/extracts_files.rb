f9514f57714a68d3eeee8f43f8dde4dfb22a7a46
Dan Milliron <dan@railsblazer.com>
2019-01-14 12:30:05 -0700

    Move a misplaced method definition; no functional change

--- app/analyzers/concerns/extracts_files.rb
+++ app/analyzers/concerns/extracts_files.rb
@@ -3,18 +3,13 @@ require 'active_support/concern'
 module ExtractsFiles
   extend ActiveSupport::Concern
 
-  class StringIOWithPath < StringIO
-    attr_reader :path
-
-    def initialize(path, *args)
-      @path = path
-      super *args
+  def fetch_file(file_path:)
+    with_file_finder do |finder|
+      finder.find(file_path: file_path)
     end
   end
 
   class FileFinder
-    include ExtractsFiles
-
     def initialize(files:, path_method:, reader:)
       @files, @path_method, @reader = files, path_method, reader
     end
@@ -22,32 +17,35 @@ module ExtractsFiles
     def find(file_path:)
       find_file(file_path: file_path, files: @files, path_method: @path_method, file_reader: @reader)
     end
-  end
 
-  def fetch_file(file_path:)
-    with_file_finder do |finder|
-      finder.find(file_path: file_path)
+    private
+
+    def find_file(file_path:, files:, path_method:, file_reader:)
+      files.rewind if files.respond_to?(:rewind)
+
+      full_paths     = files.map(&path_method)
+      regexp         = file_path.is_a?(Regexp) ? file_path : /#{file_path}\z/
+      matching_paths = full_paths.grep regexp
+      best_path      = matching_paths.sort_by { |path| [path.split('/').size, File.basename(path).size] }.first
+      return nil unless best_path
+
+      # Don't go deeper than one level down, e.g. foo/<file_path> is fine, but foo/down/<file_path> is not.
+      split_file_path = file_path.to_s.split('/')
+      # Handle Regexps that begin with /, e.g. /\/readme/i
+      split_file_path.shift if file_path.is_a?(Regexp) && split_file_path.first =~ /\\\z/
+      return nil if best_path.split('/').size > split_file_path.size + 1
+
+      content = file_reader.call(files, best_path).to_s
+      return StringIOWithPath.new(best_path, content)
     end
   end
 
-  private
+  class StringIOWithPath < StringIO
+    attr_reader :path
 
-  def find_file(file_path:, files:, path_method:, file_reader:)
-    files.rewind if files.respond_to?(:rewind)
-
-    full_paths     = files.map(&path_method)
-    regexp         = file_path.is_a?(Regexp) ? file_path : /#{file_path}\z/
-    matching_paths = full_paths.grep regexp
-    best_path      = matching_paths.sort_by { |path| [path.split('/').size, File.basename(path).size] }.first
-    return nil unless best_path
-
-    # Don't go deeper than one level down, e.g. foo/<file_path> is fine, but foo/down/<file_path> is not.
-    split_file_path = file_path.to_s.split('/')
-    # Handle Regexps that begin with /, e.g. /\/readme/i
-    split_file_path.shift if file_path.is_a?(Regexp) && split_file_path.first =~ /\\\z/
-    return nil if best_path.split('/').size > split_file_path.size + 1
-
-    content = file_reader.call(files, best_path).to_s
-    return StringIOWithPath.new(best_path, content)
+    def initialize(path, *args)
+      @path = path
+      super *args
+    end
   end
 end
