f969fc1ce9b6698a5053471e840e691d68c35bab
Dan Milliron <dan@railsblazer.com>
2019-01-15 11:02:24 -0700

    Refactor common code; no functional change

--- app/interactors/fetch_hosted_file.rb
+++ app/interactors/fetch_hosted_file.rb
@@ -21,13 +21,9 @@ class FetchHostedFile
   end
 
   def do_fetch(blob, file_path)
-    case
-    when TarBallAnalyzer.accept?(blob)
-      TarBallAnalyzer.new(blob).fetch_file(file_path: file_path)
-    when ZipFileAnalyzer.accept?(blob)
-      ZipFileAnalyzer.new(blob).fetch_file(file_path: file_path)
-    else
-      nil
-    end
+    analyzer = ExtensionVersion.pick_blob_analyzer(blob)
+    return nil unless analyzer
+
+    analyzer.fetch_file(file_path: file_path)
   end
 end
--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,2 +1,4 @@
+# If you change this list, also update the +ExtensionVersion.pick_blob_analyzer method.
+
 Rails.application.config.active_storage.analyzers.prepend TarBallAnalyzer
 Rails.application.config.active_storage.analyzers.prepend ZipFileAnalyzer
