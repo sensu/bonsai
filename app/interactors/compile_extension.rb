56a61a798277cda59d592ebeb791b3a671a3108e
Dan Milliron <dan@railsblazer.com>
2019-01-29 15:28:57 -0700

    Make CompileExtension handle hosted extensions

--- app/interactors/compile_extension.rb
+++ app/interactors/compile_extension.rb
@@ -1,5 +1,6 @@
 # Call this service to force a compilation of the given extension.
 # The compilation will be performed asynchronously.
+# This service works with GitHub-based extensions and hosted extensions.
 
 class CompileExtension
   include Interactor
@@ -8,6 +9,19 @@ class CompileExtension
   delegate :extension, to: :context
 
   def call
+    if extension.hosted?
+      compile_hosted_extension(extension)
+    else
       SyncExtensionRepoWorker.perform_async(extension.id)
+    end
+  end
+
+  private
+
+  def compile_hosted_extension(extension)
+    extension.extension_versions.each do |version|
+      next unless version.source_file.attached?
+      version.source_file.blob.analyze_later
+    end
   end
 end
