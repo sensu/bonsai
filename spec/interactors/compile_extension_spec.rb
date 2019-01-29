335839bb8241a95891aa93dc40713edb6d090de0
Dan Milliron <dan@railsblazer.com>
2019-01-29 15:28:57 -0700

    Make CompileExtension handle hosted extensions

--- spec/interactors/compile_extension_spec.rb
+++ spec/interactors/compile_extension_spec.rb
@@ -12,5 +12,25 @@ describe CompileExtension do
         subject
       end
     end
+
+    context 'with a hosted extension' do
+      let(:extension)  { create :extension, :hosted, extension_versions_count: 0 }
+      let!(:version1)  { create :extension_version, :with_source_file, extension: extension}
+      let!(:version2)  { create :extension_version, :with_source_file, extension: extension}
+
+      before do
+        extension.reload
+      end
+
+      it 'schedules each version to be re-analyzed' do
+        num_versions = extension.extension_versions.size
+        expect(num_versions.size).to be > 0
+
+        extension.extension_versions.map(&:source_file).map(&:blob).each do |blob|
+          expect(blob).to receive(:analyze_later).exactly(1).times
+        end
+        subject
+      end
+    end
   end
 end
