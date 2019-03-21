e1aa6739f184f377eb81f9e8226df2cbb37e5e2a
pzupan@gmail.com <pzupan@gmail.com>
2019-03-21 12:41:11 -0700

    fix test

--- spec/lib/semver_normalizer_spec.rb
+++ spec/lib/semver_normalizer_spec.rb
@@ -12,16 +12,17 @@ describe SemverNormalizer do
 
     it 'strips off any leading "v" character' do
       expect(subject.call("v790")).to eq '790'
-      expect(subject.call("V097")).to eq '097'
+      expect(subject.call("V097.1.0")).to eq '097.1.0'
+      expect(subject.call("V 097.1.0")).to eq '097.1.0'
     end
 
     it 'strips any leading and trailing whitespace' do
-      expect(subject.call("\t\nv790  ")).to eq '790'
+      expect(subject.call("\t\nv790.1.0  ")).to eq '790.1.0'
     end
 
-    it 'replaces pseudo dots with dots' do
-      expect(subject.call("v7-0_9.0")).to eq '7.0.9.0'
-      expect(subject.call("V0.9-7_1")).to eq '0.9.7.1'
+    it 'does not replace underscore or hypen' do
+      expect(subject.call("v7.1.0-pre")).to eq '7.1.0-pre'
+      expect(subject.call("V0.9.1-7_1")).to eq '0.9.1-7_1'
     end
   end
 end
