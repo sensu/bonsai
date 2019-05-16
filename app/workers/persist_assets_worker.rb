c5f6adfc3a4c6d8edc940e830442b5608bf6566b
pzupan@gmail.com <pzupan@gmail.com>
2019-05-16 08:13:34 -0700

    fix bug in persist asset worker

--- app/workers/persist_assets_worker.rb
+++ app/workers/persist_assets_worker.rb
@@ -3,7 +3,7 @@ class PersistAssetsWorker < ApplicationWorker
   def perform(version_id)
     @version = ExtensionVersion.find_by(id: version_id)
     raise RuntimeError.new("Version ID: #{version_id} not found.") unless @version
-    PersistAssets.call(version: version)
+    PersistAssets.call(version: @version)
   end
 
 end
