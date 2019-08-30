9ca6d2f1d500a33d8992c6e1671bd92dbdabd026
pzupan@gmail.com <pzupan@gmail.com>
2019-08-30 12:18:32 -0700

    change config annotations

--- app/controllers/concerns/annotations.rb
+++ app/controllers/concerns/annotations.rb
@@ -8,16 +8,19 @@ module Annotations
       version.release_assets.map{|a| a.labels}.flatten.reject(&:blank?).uniq.sort.join(', ')
     end
     tags ||= nil
+    owner_name = extension.hosted? ? ENV['HOST_ORGANIZATION'] : extension.owner_name
 
     annotations = {
-      'io.sensio.bonsai.url' => owner_scoped_extension_url(extension),
-      'io.sensio.bonsai.api_url' => owner_scoped_release_asset_builds_api_v1_url(extension, version),
-      'io.sensio.bonsai.tier' => extension.tier_name,
-      'io.sensio.bonsai.version' => version.version,
-      'io.sensio.bonsai.tags' => tags,
+      'io.sensu.bonsai.url' => owner_scoped_extension_url(extension),
+      'io.sensu.bonsai.api_url' => owner_scoped_api_v1_extension_url(extension),
+      'io.sensu.bonsai.tier' => extension.tier_name,
+      'io.sensu.bonsai.version' => version.version,
+      'io.sensu.bonsai.namespace' => owner_name,
+      'io.sensu.bonsai.name' => extension.lowercase_name,
+      'io.sensu.bonsai.tags' => tags,
     }
     if extension.hosted?
-      annotations['io.sensio.bonsai.message'] = "This asset is for users with a valid Enterprise license"
+      annotations['io.sensu.bonsai.message'] = "This asset is for users with a valid Enterprise license"
     end
     annotations
   end 
--- app/controllers/concerns/url_helpers.rb
+++ app/controllers/concerns/url_helpers.rb
@@ -11,6 +11,14 @@ module UrlHelpers
     end
   end
 
+  def owner_scoped_api_v1_extension_url(extension)
+    if extension.hosted?
+      api_v1_extension_url(username: "#{ENV['HOST_ORGANIZATION']}", id: extension.lowercase_name)
+    else
+      api_v1_extension_url(username: extension.owner_name, id: extension.lowercase_name)
+    end
+  end 
+
   def owner_scoped_release_asset_builds_api_v1_url(extension, version)
     if extension.hosted?
       api_v1_release_asset_builds_url(username: "#{ENV['HOST_ORGANIZATION']}", id: extension.lowercase_name, version: version.version)
