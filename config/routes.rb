3463c43b2b589b58b17b10f8ecedd8c160064d2f
pzupan@gmail.com <pzupan@gmail.com>
2020-01-14 15:52:00 -0800

    fix conflicts 2

--- config/routes.rb
+++ config/routes.rb
@@ -69,10 +69,7 @@ Rails.application.routes.draw do
         put :select_default_version
         put :update_collection
         put :privacy
-<<<<<<< HEAD
         put :update_config_overrides
-=======
->>>>>>> issue_253
       end
     end
 
