e3859f0469461ebb30310a0a212c065cf002dcfe
James Phillips <jamesdphillips@gmail.com>
2020-02-04 11:23:19 -0800

    Remove "recompile enterprise" runbook
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

--- lib/runbooks/recompile_enterprise_tier.rb
+++
@@ -1,6 +0,0 @@
-enterprise_tier = Tier.find_by(name: 'Enterprise')
-extensions = Array.wrap(enterprise_tier&.extensions)
-
-extensions.each do |extension|
-  CompileExtension.call(extension: extension)
-end
