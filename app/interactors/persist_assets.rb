2436fb5b9c234c47572a9b375646cd195667ff61
Dan Milliron <dan@railsblazer.com>
2021-12-07 20:46:14 +0000

    Refactor code; no functional change

--- app/interactors/persist_assets.rb
+++ app/interactors/persist_assets.rb
@@ -29,56 +29,58 @@ class PersistAssets
         next
       end
 
-      key = release_asset.destination_pathname
-
-      object_exists = s3_bucket.object(key).exists?
-
-      if object_exists
-        # we need to replace the file each iteration in order
-        # to update files in case they were changed.
-        begin
-          s3_bucket.object(key).delete
-          puts "Removed file from S3: #{key}"
-        rescue Aws::S3::Errors::ServiceError => error 
-          puts "****** S3 error: #{error.code} - #{error.message}"
-        end
-      end 
-
-      # get file contents
-      begin
-        file = url.open
-      rescue OpenURI::HTTPError => error
-        status = error.io.status[0]
-        message = error.io.status[1]
-        puts "****** file read error: #{status} - #{message}"
-        next
-      end
-     
-      begin
-        s3_bucket.object(key).put(body: file, acl: 'public-read')
-        puts "File saved to S3: #{key}"
-        uri = URI(s3_bucket.object(key).public_url)
-        last_modified = s3_bucket.object(key).last_modified
-      rescue Aws::S3::Errors::ServiceError => error 
-        puts "****** S3 error: #{error.code} - #{error.message}"
-        next
-      end
-
-      if ENV['AWS_S3_VANITY_HOST'].present?
-        # replace the entire host
-        uri.host = ENV['AWS_S3_VANITY_HOST']
-        # remove the bucket from the path if returned in that format
-        # note that this does not change the host
-        uri.path.gsub!(/#{ENV['AWS_S3_ASSETS_BUCKET']}\//, '')
-        puts "******** Updating vanity_url: #{uri.to_s}"
-      end
-      release_asset.update_columns(vanity_url: uri.to_s, last_modified: last_modified)
-
+      mirror_to_s3(release_asset, url)
     end # builds.each
 
   end
 
   private
+
+  def mirror_to_s3(release_asset, url)
+    key           = release_asset.destination_pathname
+    object_exists = s3_bucket.object(key).exists?
+
+    if object_exists
+      # we need to replace the file each iteration in order
+      # to update files in case they were changed.
+      begin
+        s3_bucket.object(key).delete
+        puts "Removed file from S3: #{key}"
+      rescue Aws::S3::Errors::ServiceError => error
+        puts "****** S3 error: #{error.code} - #{error.message}"
+      end
+    end
+
+    # get file contents
+    begin
+      file = url.open
+    rescue OpenURI::HTTPError => error
+      status  = error.io.status[0]
+      message = error.io.status[1]
+      puts "****** file read error: #{status} - #{message}"
+      return
+    end
+
+    begin
+      s3_bucket.object(key).put(body: file, acl: 'public-read')
+      puts "File saved to S3: #{key}"
+      uri           = URI(s3_bucket.object(key).public_url)
+      last_modified = s3_bucket.object(key).last_modified
+    rescue Aws::S3::Errors::ServiceError => error
+      puts "****** S3 error: #{error.code} - #{error.message}"
+      return
+    end
+
+    if ENV['AWS_S3_VANITY_HOST'].present?
+      # replace the entire host
+      uri.host = ENV['AWS_S3_VANITY_HOST']
+      # remove the bucket from the path if returned in that format
+      # note that this does not change the host
+      uri.path.gsub!(/#{ENV['AWS_S3_ASSETS_BUCKET']}\//, '')
+      puts "******** Updating vanity_url: #{uri.to_s}"
+    end
+    release_asset.update_columns(vanity_url: uri.to_s, last_modified: last_modified)
+  end
 
   def s3_bucket
     @_s3_bucket ||= begin
