425c5a9b91f1e711b1521c03da1b725bd63b7208
Sean Porter <portertech@gmail.com>
2023-06-27 12:03:41 -0700

    redis ssl params for verify mode, https://devcenter.heroku.com/articles/connecting-heroku-redis#connecting-in-ruby
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
 
-REDIS_POOL = ConnectionPool.new(size: 10) { Redis.new(url: "#{ENV['REDIS_URL']}/0", network_timeout: 5) }
+REDIS_POOL = ConnectionPool.new(size: 10) { Redis.new(url: "#{ENV['REDIS_URL']}/0", network_timeout: 5, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }) }
 
-Redis.current = Redis.new(url: "#{ENV['REDIS_URL']}/1", network_timeout: 5)
+Redis.current = Redis.new(url: "#{ENV['REDIS_URL']}/1", network_timeout: 5, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
