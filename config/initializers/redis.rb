df0be352b97ed2ec330c3da1e16cce2f536fb1a8
Sean Porter <portertech@gmail.com>
2023-07-06 22:05:34 -0700

    silence redis deprecation warnings
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,3 +3,4 @@ require "redis"
 REDIS_POOL = ConnectionPool.new(size: 10) { Redis.new(url: "#{ENV['REDIS_URL']}/0", network_timeout: 5, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }) }
 
 Redis.current = Redis.new(url: "#{ENV['REDIS_URL']}/1", network_timeout: 5, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+Redis.silence_deprecations = true
