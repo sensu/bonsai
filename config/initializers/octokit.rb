603adb68fd0285db48799c72fb602abeae2c9859
Rajshekar Chavakula <rchavakula.ctr@sumologic.com>
2025-04-04 18:50:49 +0530

    demo changes

--- config/initializers/octokit.rb
+++ config/initializers/octokit.rb
@@ -1,4 +1,5 @@
 Rails.application.config.octokit = Octokit::Client.new(
+  access_token: ENV["GITHUB_ACCESS_TOKEN"],
   client_id: ENV["GITHUB_CLIENT_ID"],
   client_secret: ENV["GITHUB_CLIENT_SECRET"]
 )
